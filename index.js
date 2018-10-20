479aa1914bec0d16519ea0e90689fc110d54121b
Pavol Hejny <me@pavolhejny.com>
2018-10-20 17:30:14 +0200

    ball

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ const ctx = scene.getContext('2d');
 
 function loop(time){
 
-    for(let i=0;i<500;i++){
-        (i===0?ctx.moveTo:ctx.lineTo).call(ctx,i,Math.sin(i/10)*time/100+250);
-    }
+    ctx.clearRect(0,0,scene.width,scene.height);
+    ctx.beginPath();
+
+    ctx.arc(
+        200,
+        200+time/100,
+        60,
+        0,
+        Math.PI * 2,
+        true,
+    );
+
+
+
+    /*for(let i=0;i<500;i++){
+        (i===0?ctx.moveTo:ctx.lineTo).call(ctx,i+time/100,Math.sin(i/10)*100+250);
+    }*/
+
+    ctx.fillStyle='#906090';
+    ctx.lineWidth = 5;
+
     ctx.stroke();
+    ctx.fill();
 
     requestAnimationFrame(loop);
 }
